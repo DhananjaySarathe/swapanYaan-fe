ea9129d7b4d2f636712e5c3347c39e60f145da48
axat0207 <axxatagrawal@gmail.com>
2023-09-21 11:51:46 +0530

    link and logo removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,7 @@ function Navbar() {
   return (
     <nav className="bg-blue-300  border-gray-200  w-full">
       <div className="max-w-screen-xl  flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+        <a href="/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">SwapnaYaan</span>
         </a>
         <div className="flex items-center md:order-2">
